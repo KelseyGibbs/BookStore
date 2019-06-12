993e830a9716389a1684654843c9981fb0d9e1c0
KelseyGibbs <gibbs.kelseylee@gmail.com>
2019-06-12 16:04:05 -0500

    updated react router

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,21 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
+import search from  "./pages/Search";
+import saved from  "./pages/Saved";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Hello World</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Router>
+
+        <Switch>
+          <Route exact path="/search" Component={saved} />
+          <Route exact path="/saved" Component={search} />
+          <Route from="/" to="search" /> 
+        </Switch>
+
+      </Router>
     );
   }
 }
