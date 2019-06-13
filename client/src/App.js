af679a9139174d0d16e1de2ecb9f0f35a89bea5f
KelseyGibbs <gibbs.kelseylee@gmail.com>
2019-06-12 19:31:58 -0500

    added component file structure, ajax boiler plate

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,57 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import "./App.css";
-import search from  "./pages/Search";
-import saved from  "./pages/Saved";
+import Search from  "./pages/Search";
+import Saved from  "./pages/Saved";
+
+var fetchingResource = null;
+
+function googleBooksAPI () {
+    if(fetchingResource) {
+        fetchingResource.abort();
+    }
+    fetchingResource = $.ajax({
+       url: '/resource',
+       data: {},
+       cache: false,
+       timeout: 15000,
+       type: 'GET',
+       beforeSend: function () {},
+       complete: function () {
+           fetchingResource = null;
+       },
+       error: function () {
+           if (result.statusText != "abort");
+       },
+       success: function () {}
+    }); 
+}
+
 
 class App extends Component {
   render() {
     return (
       <Router>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">GoogleBooks</Link>
+          </li>
+          <li>
+            <Link to="/Search">About</Link>
+          </li>
+          <li>
+            <Link to="/Saved">Topics</Link>
+          </li>
+        </ul>
 
-        <Switch>
-          <Route exact path="/search" Component={saved} />
-          <Route exact path="/saved" Component={search} />
-          <Route from="/" to="search" /> 
-        </Switch>
+        <hr />
 
-      </Router>
+        <Route exact path="/" component={Search} />
+        <Route path="/saved" component={Saved} />
+        <Route path="/search" component={Search} />
+      </div>
+    </Router>
     );
   }
 }
