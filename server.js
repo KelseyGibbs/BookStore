d2e58393ec299d2fe4a370a266f817f2a3e8e73d
KelseyGibbs <gibbs.kelseylee@gmail.com>
2019-06-15 19:05:29 -0500

    updated mlab

--- server.js
+++ server.js
@@ -12,22 +12,18 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Serve up static assets (usually on heroku)
-app.listen(process.env.PORT || 3000, function(){
-  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-});
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
 
 app.use(routes);
 
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+//   Connect to the Mongo DB
+let MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/googlebooks";
 
-mongoose.connect(MONGODB_URI);
-
-// //   Connect to the Mongo DB
-// let MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/googlebooks";
-
-// mongoose.connect(MONGODB_URI, {
-//     useNewUrlParser: true
-// });
+mongoose.connect(MONGODB_URI, {
+    useNewUrlParser: true
+});
 
 app.listen(PORT, () => {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
