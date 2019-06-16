8f90e3b82d379edafe1d73d96c45aaa0285c114f
KelseyGibbs <gibbs.kelseylee@gmail.com>
2019-06-15 19:12:52 -0500

    updated route

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ if (process.env.NODE_ENV === "production") {
 app.use(routes);
 
 //   Connect to the Mongo DB
-let MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/googlebooks";
+let MONGODB_URI = process.env.MONGOLAB_COBALT_URI || "mongodb://localhost/googlebooks";
 
 mongoose.connect(MONGODB_URI, {
     useNewUrlParser: true
